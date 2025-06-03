0e1d9f58fa3765a2311f6e5870f7f1923f1efdd2
pedrodespessel <91088032+pedrodespessel@users.noreply.github.com>
2025-06-03 14:14:25 -0300

    funcionalidade de menu

--- aula/cadastro.py
+++ aula/cadastro.py
@@ -8,22 +8,38 @@ if os.path.exists('produtos.json'):
 else:
     produtos = []  # Se não existir, começa com lista vazia'
 
-while True:
-    nome = input('insira o nome:')
-    preco = float(input('insira o preço:'))
-    produtos.append({
-        'nome': nome,
-        'preco': preco
-    })
-    continuar = input('deseja cadastrar mais? (s/n)')
-    if continuar.lower() == 'n':
-        break
+def cadastrar_produtos():
+    while True:
+        nome = input('insira o nome:')
+        preco = float(input('insira o preço:'))
+        produtos.append({
+            'nome': nome,
+            'preco': preco
+        })
+        continuar = input('deseja cadastrar mais? (s/n)')
+        if continuar.lower() == 'n':
+            break
+        # Cria o arquivo json - propriedade 'w' é pra escrever, 'r' é pra ler
+    with open('produtos.json', 'w', encoding='utf-8') as f:
+        json.dump(produtos, f, ensure_ascii=False, indent=4)
+        print('Banco de Dados atualizado com Sucesso!')
 
-# Cria o arquivo json - propriedade 'w' é pra escrever, 'r' é pra ler
-with open('produtos.json', 'w', encoding='utf-8') as f:
-    json.dump(produtos, f, ensure_ascii=False, indent=4)
-print('Arquivo Criado com Sucesso!')
+def listar_produtos():
+    print('\n\nprodutos cadastrados:')
+    for produto in produtos:
+        print(f'nome: {produto['nome']}, preço: {produto['preco']}')
 
-print('produtos cadastrados:')
-for produto in produtos:
-    print(f'nome: {produto['nome']}, preço: {produto['preco']}')
+def exibir_menu():
+    print('\n\n\n\nSistema de Cadastro de Produtos!\n')
+    print('1. Cadastrar produto')
+    print('2. Listar produtos')
+    print('3. Sair')
+    escolha = int(input('Escolha uma opção:'))
+    if escolha == 1:
+        cadastrar_produtos()
+    elif escolha == 2:
+        listar_produtos()
+    else:
+        print('você saiu do menu!')
+
+exibir_menu()
